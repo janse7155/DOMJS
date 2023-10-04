ae260a056ece4cdd922699016b6fcfdee897c75a
Jaedan Seabaugh <janse7155@gmail.com>
2023-10-04 13:14:03 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    myDiv.style.backgroundColor = "lightblue";
+    let p = document.getElementById('p').innerText;
+    let p2 = document.getElementById('p2').innerText;
+    console.log(p, p2);
+});
+
